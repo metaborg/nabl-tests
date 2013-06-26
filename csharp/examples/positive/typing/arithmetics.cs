efea940ec0b47d1020eca0bf8cb88977c6bffc6e
Guido Wachsmuth <g.h.wachsmuth@tudelft.nl>
2013-06-26 09:59:03 +0200

    more binops

--- csharp/examples/positive/typing/arithmetics.cs
+++ csharp/examples/positive/typing/arithmetics.cs
@@ -1,9 +1,36 @@
 namespace arithmetics {
   
   class Arithmetics {
+    int i;
+    bool b;
+    string s;
     
-    int m() {
-      return 1 + 1;
+    void m() {
+    
+      s = "s" + "s";
+      s = 1   + "s";
+      s = "s" + 1;
+      
+      i = 1 + 1;
+      i = 1 - 1;
+      i = 1 * 1;
+      i = 1 / 1;
+    
+      b = 1 <  1;
+      b = 1 <= 1;
+      b = 1 >= 1;
+      b = 1 >  1;
+      
+      b = "s" <  "s";
+      b = "s" <= "s";
+      b = "s" >= "s";
+      b = "s" >  "s";
+      
+      b = 1    == 1;  
+      b = "s"  == "s";  
+      b = "s"  == 1;
+      b = 1    == "s";
+      b = true == false;
     }
   }
 }
