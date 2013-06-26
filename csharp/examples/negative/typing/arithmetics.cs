73652b5897a911f8411a52e5d6e9951525316631
Guido Wachsmuth <g.h.wachsmuth@tudelft.nl>
2013-06-26 10:36:08 +0200

    fixed indentation

--- csharp/examples/negative/typing/arithmetics.cs
+++ csharp/examples/negative/typing/arithmetics.cs
@@ -1,9 +1,52 @@
 namespace arithmetics {
   
   class Arithmetics {
+    int i;
+    bool b;
+    string s;
     
-    int m() {
-      return 1 + true;
+    void m() {
+    
+      s = "s"  + true;
+      s = true + "s";
+      
+      i = true + true;
+      i = 1    + true;
+      i = true + 1;
+      i = true - true;
+      i = 1    - true;
+      i = true - 1;
+      i = true * true;
+      i = 1    * true;
+      i = true * 1;
+      i = true / true;
+      i = 1    / true;
+      i = true / 1;
+      
+      b = true <  true;
+      b = 1    <  true;
+      b = true <  1;
+      b = "s"  <  true;
+      b = true <  "s";
+      b = true <= true;
+      b = 1    <= true;
+      b = true <= 1;
+      b = "s"  <= true;
+      b = true <= "s";
+      b = true >  true;
+      b = 1    >  true;
+      b = true >  1;
+      b = "s"  >  true;
+      b = true >  "s";
+      b = true >= true;
+      b = 1    >= true;
+      b = true >= 1;
+      b = "s"  >= true;
+      b = true >= "s";
+      b = 1    == true;
+      b = true == 1;
+      b = "s"  == true;
+      b = true == "s";
     }
   }
 }
