779474f3af61883383a105fd4ece60ca1cdcc4e8
Gabriel Konat <gabrielkonat@gmail.com>
2013-07-02 16:41:15 -0700

    Fix duplicate Arithmetics error.

--- csharp/examples/positive/typing/arithmetics.cs
+++ csharp/examples/positive/typing/arithmetics.cs
@@ -1,44 +1,45 @@
 namespace arithmetics {
-  
-  class Arithmetics {
-    int i;
-    bool b;
-    string s;
-    
-    void m() {
-    
-      s = "s" + "s";
-      s = 1   + "s";
-      s = "s" + 1;
-      
-      i = 1 + 1;
-      i = 1 - 1;
-      i = 1 * 1;
-      i = 1 / 1;
-    
-      b = 1 <  1;
-      b = 1 <= 1;
-      b = 1 >= 1;
-      b = 1 >  1;
-      
-      b = "s" <  "s";
-      b = 1   <  "s";
-      b = "s" <  1;
-      b = "s" <= "s";
-      b = 1   <= "s";
-      b = "s" <= 1;
-      b = "s" >= "s";
-      b = 1   >= "s";
-      b = "s" >= 1;
-      b = "s" >  "s";
-      b = 1   >  "s";
-      b = "s" >  1;
-      
-      b = 1    == 1;  
-      b = "s"  == "s";  
-      b = "s"  == 1;
-      b = 1    == "s";
-      b = true == false;
+  namespace positive {
+	  class Arithmetics {
+	    int i;
+	    bool b;
+	    string s;
+	    
+	    void m() {
+	    
+	      s = "s" + "s";
+	      s = 1   + "s";
+	      s = "s" + 1;
+	      
+	      i = 1 + 1;
+	      i = 1 - 1;
+	      i = 1 * 1;
+	      i = 1 / 1;
+	    
+	      b = 1 <  1;
+	      b = 1 <= 1;
+	      b = 1 >= 1;
+	      b = 1 >  1;
+	      
+	      b = "s" <  "s";
+	      b = 1   <  "s";
+	      b = "s" <  1;
+	      b = "s" <= "s";
+	      b = 1   <= "s";
+	      b = "s" <= 1;
+	      b = "s" >= "s";
+	      b = 1   >= "s";
+	      b = "s" >= 1;
+	      b = "s" >  "s";
+	      b = 1   >  "s";
+	      b = "s" >  1;
+	      
+	      b = 1    == 1;  
+	      b = "s"  == "s";  
+	      b = "s"  == 1;
+	      b = 1    == "s";
+	      b = true == false;
+	    }
     }
   }
 }
