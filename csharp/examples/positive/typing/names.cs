794e30840049b45684fea8ea2c7dde4c065feb92
Guido Wachsmuth <g.h.wachsmuth@tudelft.nl>
2013-07-03 04:40:52 +0200

    start for testing types of names

---
+++ csharp/examples/positive/typing/names.cs
@@ -0,0 +1,16 @@
+namespace TypesOfNames {
+  
+  class Names {
+    bool   b;
+    int    i;
+    string s;
+    
+    void m() {
+      bool   vb;
+      int    vi;
+      string vs;
+      
+      s = i + i;
+    }
+  }
+}
