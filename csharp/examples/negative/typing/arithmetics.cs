cb90d0fab1252cbd95152fb6b897b3914988746f
Guido Wachsmuth <g.h.wachsmuth@tudelft.nl>
2013-07-03 04:39:11 +0200

    extra scoping

--- csharp/examples/negative/typing/arithmetics.cs
+++ csharp/examples/negative/typing/arithmetics.cs
@@ -1,52 +1,54 @@
-namespace arithmetics {
-  
-  class Arithmetics {
-    int i;
-    bool b;
-    string s;
-    
-    void m() {
-    
-      s = "s"  + true;
-      s = true + "s";
-      
-      i = true + true;
-      i = 1    + true;
-      i = true + 1;
-      i = true - true;
-      i = 1    - true;
-      i = true - 1;
-      i = true * true;
-      i = 1    * true;
-      i = true * 1;
-      i = true / true;
-      i = 1    / true;
-      i = true / 1;
-      
-      b = true <  true;
-      b = 1    <  true;
-      b = true <  1;
-      b = "s"  <  true;
-      b = true <  "s";
-      b = true <= true;
-      b = 1    <= true;
-      b = true <= 1;
-      b = "s"  <= true;
-      b = true <= "s";
-      b = true >  true;
-      b = 1    >  true;
-      b = true >  1;
-      b = "s"  >  true;
-      b = true >  "s";
-      b = true >= true;
-      b = 1    >= true;
-      b = true >= 1;
-      b = "s"  >= true;
-      b = true >= "s";
-      b = 1    == true;
-      b = true == 1;
-      b = "s"  == true;
-      b = true == "s";
-    }
-  }
+namespace negative {
+	namespace arithmetics {
+	  
+	  class Arithmetics {
+	    int    i;
+	    bool   b;
+	    string s;
+	    
+	    void m() {
+	    
+	      s = "s"  + true;
+	      s = true + "s";
+	      
+	      i = true + true;
+	      i = 1    + true;
+	      i = true + 1;
+	      i = true - true;
+	      i = 1    - true;
+	      i = true - 1;
+	      i = true * true;
+	      i = 1    * true;
+	      i = true * 1;
+	      i = true / true;
+	      i = 1    / true;
+	      i = true / 1;
+	      
+	      b = true <  true;
+	      b = 1    <  true;
+	      b = true <  1;
+	      b = "s"  <  true;
+	      b = true <  "s";
+	      b = true <= true;
+	      b = 1    <= true;
+	      b = true <= 1;
+	      b = "s"  <= true;
+	      b = true <= "s";
+	      b = true >  true;
+	      b = 1    >  true;
+	      b = true >  1;
+	      b = "s"  >  true;
+	      b = true >  "s";
+	      b = true >= true;
+	      b = 1    >= true;
+	      b = true >= 1;
+	      b = "s"  >= true;
+	      b = true >= "s";
+	      b = 1    == true;
+	      b = true == 1;
+	      b = "s"  == true;
+	      b = true == "s";
+	    }
+	  }
+	}
 }
--- csharp/examples/positive/typing/arithmetics.cs
+++ csharp/examples/positive/typing/arithmetics.cs
@@ -1,44 +1,47 @@
-namespace arithmetics {
+namespace positive {
+
+  namespace arithmetics {
   
-  class Arithmetics {
-    int i;
-    bool b;
-    string s;
-    
-    void m() {
-    
-      s = "s" + "s";
-      s = 1   + "s";
-      s = "s" + 1;
+    class Arithmetics {
+      int i;
+      bool b;
+      string s;
       
-      i = 1 + 1;
-      i = 1 - 1;
-      i = 1 * 1;
-      i = 1 / 1;
-    
-      b = 1 <  1;
-      b = 1 <= 1;
-      b = 1 >= 1;
-      b = 1 >  1;
+      void m() {
       
-      b = "s" <  "s";
-      b = 1   <  "s";
-      b = "s" <  1;
-      b = "s" <= "s";
-      b = 1   <= "s";
-      b = "s" <= 1;
-      b = "s" >= "s";
-      b = 1   >= "s";
-      b = "s" >= 1;
-      b = "s" >  "s";
-      b = 1   >  "s";
-      b = "s" >  1;
+        s = "s" + "s";
+        s = 1   + "s";
+        s = "s" + 1;
+        
+        i = 1 + 1;
+        i = 1 - 1;
+        i = 1 * 1;
+        i = 1 / 1;
       
-      b = 1    == 1;  
-      b = "s"  == "s";  
-      b = "s"  == 1;
-      b = 1    == "s";
-      b = true == false;
+        b = 1 <  1;
+        b = 1 <= 1;
+        b = 1 >= 1;
+        b = 1 >  1;
+        
+        b = "s" <  "s";
+        b = 1   <  "s";
+        b = "s" <  1;
+        b = "s" <= "s";
+        b = 1   <= "s";
+        b = "s" <= 1;
+        b = "s" >= "s";
+        b = 1   >= "s";
+        b = "s" >= 1;
+        b = "s" >  "s";
+        b = 1   >  "s";
+        b = "s" >  1;
+        
+        b = 1    == 1;  
+        b = "s"  == "s";  
+        b = "s"  == 1;
+        b = 1    == "s";
+        b = true == false;
+      }
     }
   }
 }
